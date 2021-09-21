1dc68936ec374645bd81554d9476b77b892869b0
Byron Salty <byron@saltylabs.com>
2021-09-21 18:24:44 -0400

    1) Broadcast message on connect / disconnect

--- index.js
+++ index.js
@@ -7,9 +7,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+connectedUsers = {};
+
+function sysMsg(msg) {
+  return {
+    user: "system",
+    content: msg
+  }
+}
+
 io.on('connection', (socket) => {
+  //io.emit('chat message', "User connected");
+
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    //io.emit('chat message', sysMsg("user disconnected"));
+    console.log("someone disconnected " + socket.id);
+    let user = connectedUsers[socket.id];
+    console.log("someone disconnected " + user);
+    io.emit('chat message', sysMsg(user + " disconnected"));
+  });
+  socket.on('username', user => {
+    console.log("username registered: " + user);
+    io.emit('chat message', user + " connected");
+    connectedUsers[socket.id] = user
+    console.log("users: " + JSON.stringify(connectedUsers));
   });
 });
 
