7f63d8651d69859de9d021c4fbfc8c415ece4b73
Byron Salty <byron@saltylabs.com>
2021-09-21 22:16:45 -0400

    5 and 6) Added ability to get user list and private messaging

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
 
 connectedUsers = {};
 
+function findSocketId(username) {
+  for (const socket in connectedUsers) {
+    if (connectedUsers[socket] === username) {
+      return socket;
+    }
+  }
+}
 function sysMsg(msg) {
   return {
     user: SYSTEM_USER,
@@ -21,7 +28,34 @@ io.on('connection', (socket) => {
   //io.emit('chat message', "User connected");
 
   socket.on('chat message', msg => {
-    socket.broadcast.emit('chat message', msg);
+    
+    
+    if (/^\//.test(msg.content)) {
+      console.log("User sent a command");
+      const commandRe = /^\/(\w+) ?(\w*) ?(.*)/;
+      let found = msg.content.match(commandRe);
+
+      if (found[1] === "list") {
+        msg = sysMsg("Users: " + Object.values(connectedUsers));
+        
+        socket.emit('chat message', msg);
+      } else if (found[1] === "msg") {
+        toPerson = found[2]
+        body = found[3]
+        id = findSocketId(toPerson);
+        if (id) {
+          msg.content = body;
+          socket.to(id).emit('chat message', msg);
+        } else {
+          socket.emit('chat message', sysMsg("Private Message not deliverable."));
+        }
+      } else {
+        console.log("Unknown command");
+        socket.emit('chat message', sysMsg("Unknown command: " + found[1]));
+      }
+    } else {
+      socket.broadcast.emit('chat message', msg);
+    }
   });
   socket.on('typing', msg => {
     console.log(msg.user + " is typing");
